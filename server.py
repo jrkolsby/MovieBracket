c72c2dd864a5df60c00aaec25bed6a65eabbfd0e
James Kolsby <jrkolsby@mac.com>
2018-10-08 14:13:46 -0400

    fix: began implementing traversal

--- server.py
+++ server.py
@@ -25,7 +25,8 @@ def error(payload):
     return jsonify(response)
 
 class Match():
-    def __init__(self, titleA, titleB):
+
+    def __init__(self, titleA, titleB, depth=0):
         self.exists = True
 
         self.titleA = titleA
@@ -34,6 +35,7 @@ class Match():
         self.awardsB = getAwards(titleB)
 
         self.win = False # True :: A wins
+        self.depth = depth
 
     def winner(self):
         if len(self.awardsA) > len(self.awardsB):
@@ -52,6 +54,36 @@ def index():
 def add():
     return "add"
 
+def makeBracket(inputs):
+    inputSize = len(inputs)
+
+    bracketWidth = int(pow(2, math.ceil(math.log(inputSize,2))))
+    bracketSize = bracketWidth * 2 # - 1
+
+    bracket = [None] * bracketSize
+
+    for i in range(inputSize):
+        bracket[bracketSize-1-i] = inputs[i]
+
+    for i in range(bracketSize-2, 1, -2):
+        match = bracket[i]
+        sibling = bracket[i+1]
+
+        if (match is not None and sibling is not None):
+            parent = Match(sibling.winner(), match.winner(), match.depth+1)
+            bracket[i/2] = parent
+
+        if (match is None and sibling is not None):
+            jump = Match(sibling.titleA, sibling.titleB, sibling.depth+1)
+            jump.exists = False
+            bracket[i/2] = jump
+
+    return bracket;
+
+def inOrder(bracket):
+    root = bracket[1]
+    return bracket;
+
 # READ
 @app.route("/results/", methods=['GET', 'POST', 'DELETE'])
 def getResults():
@@ -68,39 +100,17 @@ def getResults():
     for i in range(0, len(thisRound), 2):
         inputs.append(Match(thisRound[i], thisRound[i+1]))
 
-    inputSize = len(inputs)
+    bracket = makeBracket(inputs)
 
-    bracketWidth = int(pow(2, math.ceil(math.log(inputSize,2))))
-    bracketSize = bracketWidth * 2 # - 1
-
-    bracket = [None] * bracketSize
-
-    for i in range(inputSize):
-        bracket[bracketSize-1-i] = inputs[i]
-
-    for i in range(bracketSize-2, 1, -2):
-        print i
-        match = bracket[i]
-        sibling = bracket[i+1]
-
-        if (match is not None and sibling is not None):
-            bracket[i/2] = Match(match.winner(), sibling.winner()) 
-
-        if (match is None and sibling is not None):
-            fake = Match(sibling.titleA, sibling.titleB)
-            fake.exists = False
-            bracket[i/2] = fake
-
-    pp.pprint(list(map(lambda m: (m.titleA + ' vs ' + m.titleB) if (m is not None and m.exists) else '', bracket)))
-
-    return error(bracketSize)
+    prettyBracket = inOrder(bracket)
     
-    '''
+    pp.pprint(prettyBracket)
+    #pp.pprint(list(map(lambda m: str(m.depth) + ': ' + m.titleA + ' vs ' + m.titleB if m is not None and m.exists else '', bracket)))
+     
     return success({
-        "input": render_template('input.html', movies=inputs, edges=inputEdges),
-        "results": render_template('results.html', size=thisSize, bracket=results) 
+        "input": render_template('results.html', matches=inputs),
+        "results": render_template('results.html', matches=prettyBracket) 
     });
-    '''
 
 # UPDATE
 @app.route("/db/update")
